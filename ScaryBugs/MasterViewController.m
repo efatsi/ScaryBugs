58cfc8f5422a88ed6ee8c5a0d68a582e7005c791
Elias Fatsi <efatsi@comcast.net>
2012-08-09 16:29:17 -0600

    about to start tutorial 3/3

--- ScaryBugs/MasterViewController.m
+++ ScaryBugs/MasterViewController.m
@@ -31,10 +31,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] 
+                                              initWithBarButtonSystemItem:UIBarButtonSystemItemAdd 
+                                              target:self action:@selector(addTapped:)];
     
     self.title = @"Scary Bugs";
 }
