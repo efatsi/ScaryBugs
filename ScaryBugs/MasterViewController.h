6fd7ffe865e89b873720671624fb8510f43c5f97
Elias Fatsi <efatsi@comcast.net>
2012-08-09 12:40:28 -0600

    Initial Commit

---
+++ ScaryBugs/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ScaryBugs
+//
+//  Created by Viget on 8/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
